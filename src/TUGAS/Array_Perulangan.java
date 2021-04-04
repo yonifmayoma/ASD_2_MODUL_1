6ab55f3f0fe8469f8df698ab4a28d37d747771c4
Mayonice <Mayonice@LAPTOP-02UJB4GM>
2021-04-04 21:59:09 +0700

    TUGAS 1

---
+++ src/TUGAS/Array_Perulangan.java
@@ -0,0 +1,10 @@
+package TUGAS;
+public class Array_Perulangan {
+    public static void main(String[] args){
+        String[] siswa = {"Reinan", "Odena", "Geanno"}; //panjang array 3
+        
+            for (int i=0; i < siswa.length; i++){
+            System.out.println("Indeks ke " + i + " = " + siswa[i]);
+            }
+    }
+}
---
+++ src/TUGAS/NO_2.java
@@ -0,0 +1,13 @@
+package TUGAS;
+public class NO_2 {
+    public static void main (String[] args){
+        
+        int x,y;
+        for (x=0; x<=4; x++){
+            for (y=0; y<x; y++){
+                System.out.print(x);
+            }
+        System.out.println ();
+            }
+        }
+}
